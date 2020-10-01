ef46c144f1b0772f0608fe4601ae8eea8dc82f3b
Joe Finix <joefinixcode@gmail.com>
2020-10-01 18:34:03 +0800

    Done

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@
 // console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-var numbers = [3, 56, 2, 48, 5];
+// var numbers = [3, 56, 2, 48, 5];
 
 // var newNumber = 0;
 // numbers.forEach(function (currentNumber) {
@@ -58,14 +58,35 @@ var numbers = [3, 56, 2, 48, 5];
 
 // console.log(newNumber);
 
-var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-  console.log('accumulator = ' + accumulator);
-  console.log('currentNumber = ' + currentNumber);
-  return accumulator + currentNumber;
-});
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//   console.log('accumulator = ' + accumulator);
+//   console.log('currentNumber = ' + currentNumber);
+//   return accumulator + currentNumber;
+// });
 
-console.log(newNumber);
+// console.log(newNumber);
 
 //Find - find the first item that matches from an array.
+// var numbers = [3, 56, 2, 48, 5];
+
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
 
 //FindIndex - find the index of the first item that matches.
+// var numbers = [3, 56, 2, 48, 5];
+
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// });
+
+// console.log(newNumber);
+
+import emojipedia from './emojipedia';
+
+const newMeaning = emojipedia.map(function (x) {
+  return x.meaning.substring(0, 100);
+});
+console.log(newMeaning);
